e5ae2bdac4e3e6edae3aeba4c505453cccd521ed
Damla Erva Kasal <damlaervakasal3@gmail.com>
2024-11-22 15:28:07 +0300

    chore: version update and necessary fixes applied

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
   content: [
     "./app/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
+    "./hooks/**/*.{js,ts,jsx,tsx}",
+    "./screens/**/*.{js,ts,jsx,tsx}",
   ],
   presets: [require("nativewind/preset")],
   darkMode: "class",
