aa9fb2c94ec1a1d9213792fca5d17a48110a7e5d
Damla Erva Kasal <damlaervakasal3@gmail.com>
2024-11-27 01:52:46 +0300

    ui: updated tab bar height and style settings

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
+import { Dimensions } from 'react-native';
 
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
@@ -11,6 +11,10 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
+  const { height: screenHeight } = Dimensions.get('window');
+
+  const tabBarHeight = screenHeight * 0.08;
+
   return (
     <Tabs
       screenOptions={{
@@ -18,33 +22,20 @@ export default function TabLayout() {
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
+        tabBarStyle: [
+          { position: 'absolute' },
+           { height: tabBarHeight },
+         ],
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'idk',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="new"
-        options={{
-          title: 'new',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Search for Ricky and Morty Characters',
+          tabBarIcon: () => null,
+          tabBarLabelStyle: {
+            fontSize: 16,
+            marginTop: -15,
+          }          
         }}
       />
     </Tabs>
