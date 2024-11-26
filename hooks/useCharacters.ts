31abcbe09a6131b36125935c70f888b0cf08d632
Damla Erva Kasal <damlaervakasal3@gmail.com>
2024-11-27 01:44:18 +0300

    feat: added custom hook to fetch characters using React Query

--- hooks/useCharacters.ts
+++ hooks/useCharacters.ts
@@ -1,11 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 import { getCharacters } from '@/api/rickAndMorty';
+import { Character } from '@/types/types';
 
-const useCharacters = (query: string) => {
-  return useQuery({
+const useCharacters = (query: string = '') => {
+  return useQuery<Character[], Error>({
     queryKey: ['characters', query],
     queryFn: () => getCharacters(query),
-    enabled: !!query,
   });
 };
 
